a933bcd835a3c78b5ac606e95220c3698ad260c2
wwwdotphilip <wwwdotphilip@gmail.com>
2017-10-06 15:01:20 +0800

    Use unit testing

--- app/src/androidTest/java/au/com/bywave/randomizer/RandomizerInstrumentedTest.java
+++ app/src/androidTest/java/au/com/bywave/randomizer/RandomizerInstrumentedTest.java
@@ -1,9 +1,14 @@
 package au.com.bywave.randomizer;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingPolicies;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -16,8 +21,32 @@ public class RandomizerInstrumentedTest {
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
+//    @Test
+//    public void mainTest() {
+//        onView(withId(R.id.tvResult)).check(matches(isDisplayed())).perform(click());
+//    }
+
     @Test
-    public void mainTest() {
-        onView(withId(R.id.btnSet)).check(matches(isDisplayed())).perform(click());
+    public void waitForWork() {
+        long waitingTime = 10000;
+
+        // Start
+        onView(withId(R.id.tvResult)).perform(click());
+
+        // Make sure Espresso does not time out
+        IdlingPolicies.setMasterPolicyTimeout(
+                waitingTime * 2, TimeUnit.MILLISECONDS);
+        IdlingPolicies.setIdlingResourceTimeout(
+                waitingTime * 2, TimeUnit.MILLISECONDS);
+
+        // Now we wait
+        IdlingResource idlingResource = new ElapsedTimeIdlingResource(waitingTime);
+        Espresso.registerIdlingResources(idlingResource);
+
+        // Stop
+        onView(withId(R.id.tvResult)).check(matches(isDisplayed()));
+
+        // Clean up
+        Espresso.unregisterIdlingResources(idlingResource);
     }
 }
